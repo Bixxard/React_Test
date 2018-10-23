7abb0d51dd63992f7785a0fb3616b90e6b0c886d
Bixxard <mixa-1997@tut.by>
2018-10-23 11:42:08 +0300

    add serch from title

--- src/components/Table/TableBody.js
+++ src/components/Table/TableBody.js
@@ -1,18 +1,15 @@
 import React, { Component } from 'react';
 
 // CHILD ROW
-import TableBodyRow from './TableBodyRow';
-// IMPLEMENT THIS PURE COMPONENT TO OUR TABLE
 
+
+import TableBodyRow from './TableBodyRow';
 class TableBody extends Component {
     render() {
-        const { data, isCheckboxActive, activeRowHanlder, activeRowID } = this.props;
-        const TableBodyRow = data
-            // TODO SINGLE SEARCH AS WELL
-            // TODO MAKE MULTIPLY OBJECT SEARCH
+        const { data, isCheckboxActive, activeRowHanlder, activeRowID, searchValue } = this.props;
+        const TableBodyRows = data
+            .filter(item => item.title.indexOf(searchValue) > -1)
             .map(item => (
-            // TODO MAKE REFACTORING WITH SCSS
-            // TODO https://medium.com/@oreofeolurin/configuring-scss-with-react-create-react-app-1f563f862724
             <tr style={{ background: activeRowID.indexOf(item.id) > -1 ? "red" : "" }}  key={item.id}>
                 <td style={{ display: !isCheckboxActive ? "none" : "block" }}>
                     <input
@@ -20,19 +17,20 @@ class TableBody extends Component {
                         type="checkbox"
                     />
                 </td>
-                <td>{item.id}</td>
-                <td>{item.title}</td>
+                <TableBodyRow id={item.id}
+                />
+                <TableBodyRow id={item.title}
+                />
             </tr>
             )
         );
+        console.log(searchValue)
         return (
             <tbody>
-                    {TableBodyRow}
+                    {TableBodyRows}
             </tbody>
         );
     }
 }
-
-
 export default TableBody;
 
--- src/components/Table/TableHeaderRow.js
+++ src/components/Table/TableHeaderRow.js
@@ -1,9 +1,7 @@
 import React from 'react';
-
 const TableHeaderRow = (props) => {
     return(
         <th>{props.name}</th>
     )
 };
-
 export default TableHeaderRow;
